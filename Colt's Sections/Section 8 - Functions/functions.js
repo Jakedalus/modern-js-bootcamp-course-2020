2701d5a8a18a4fc6ec11659da3593313f3c70605
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-08-07 15:48:14 -0400

    Add an alt version to isPasswordValid

--- Colt's Sections/Section 8 - Functions/functions.js
+++ Colt's Sections/Section 8 - Functions/functions.js
@@ -17,3 +17,21 @@ console.log(isValidPassword('dogluvr69', 'dogluvr')); // false (contains usernam
 console.log(isValidPassword('francis bacon', 'joe')); // false (contains space)
 console.log(isValidPassword('francis   bacon', 'joe')); // false (contains spaces)
 console.log(isValidPassword('yo', 'joe')); // false (too short)
+
+function anotherVersion(password, username) {
+	if (
+		password.length < 8 ||
+		password.includes(' ') ||
+		password.includes(username)
+	) {
+		return false;
+	}
+
+	return true;
+}
+
+console.log(anotherVersion('9082309uio0293u', 'francis')); // true
+console.log(anotherVersion('dogluvr69', 'dogluvr')); // false (contains username)
+console.log(anotherVersion('francis bacon', 'joe')); // false (contains space)
+console.log(anotherVersion('francis   bacon', 'joe')); // false (contains spaces)
+console.log(anotherVersion('yo', 'joe')); // false (too short)
