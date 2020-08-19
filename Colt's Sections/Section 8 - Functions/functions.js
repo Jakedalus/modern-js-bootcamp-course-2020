0eeb518c2285a6482db38d736d2540b73e747ce0
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-08-19 15:15:05 -0400

    Add solution to 'average' function

--- Colt's Sections/Section 8 - Functions/functions.js
+++ Colt's Sections/Section 8 - Functions/functions.js
@@ -1,3 +1,6 @@
+/////// PASSWORD ///////
+console.log('/////// PASSWORD ///////');
+
 function isValidPassword(password, username) {
 	const isAtLeastEightChars = password.length >= 8;
 	const hasNoSpaces = !password.includes(' ');
@@ -18,7 +21,7 @@ console.log(isValidPassword('francis bacon', 'joe')); // false (contains space)
 console.log(isValidPassword('francis   bacon', 'joe')); // false (contains spaces)
 console.log(isValidPassword('yo', 'joe')); // false (too short)
 
-function anotherVersion(password, username) {
+function anotherVersionOfValidPW(password, username) {
 	if (
 		password.length < 8 ||
 		password.includes(' ') ||
@@ -30,8 +33,35 @@ function anotherVersion(password, username) {
 	return true;
 }
 
-console.log(anotherVersion('9082309uio0293u', 'francis')); // true
-console.log(anotherVersion('dogluvr69', 'dogluvr')); // false (contains username)
-console.log(anotherVersion('francis bacon', 'joe')); // false (contains space)
-console.log(anotherVersion('francis   bacon', 'joe')); // false (contains spaces)
-console.log(anotherVersion('yo', 'joe')); // false (too short)
+console.log(
+	anotherVersionOfValidPW('9082309uio0293u', 'francis')
+); // true
+console.log(
+	anotherVersionOfValidPW('dogluvr69', 'dogluvr')
+); // false (contains username)
+console.log(
+	anotherVersionOfValidPW('francis bacon', 'joe')
+); // false (contains space)
+console.log(
+	anotherVersionOfValidPW('francis   bacon', 'joe')
+); // false (contains spaces)
+console.log(anotherVersionOfValidPW('yo', 'joe')); // false (too short)
+
+/////// AVERAGE ///////
+console.log('/////// AVERAGE ///////');
+
+function average(arr) {
+	let sum = 0;
+	arr.forEach(n => (sum += n));
+	return sum / arr.length;
+}
+
+console.log('[1,2,3]', average([ 1, 2, 3 ]));
+console.log('[10,20,30]', average([ 10, 20, 30 ]));
+console.log('[7,12,56]', average([ 7, 12, 56 ]));
+
+/////// PANGRAMS ///////
+console.log('/////// PANGRAMS ///////');
+
+/////// PLAYING CARDS ///////
+console.log('/////// PLAYING CARDS ///////');
